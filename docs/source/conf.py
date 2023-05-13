840271698a6982bce4051813f63bdea4f9dffaa2
caotrido <cao-tri@mazars.fr>
2023-05-13 23:34:21 +0200

    [MODIFIED] Remove _static folder from path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -69,7 +69,8 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
+# html_static_path = ['_static']
 
 # By default, generated HTML <title> has the value project release documentation. E.g. Tech writer at work 1.0 documentation.
 # If you don’t have conf.py’s release set, it is e.g., Tech writer at work  documentation (please note two spaces among “work” and “documentation”).
